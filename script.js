ecda39bfe5ab510d84f9441ef78cf25133a9b680
Sanjaya Thapa <thapasanjay45643@gmail.com>
2026-01-31 19:52:03 +0545

    handled data

--- script.js
+++ script.js
@@ -1,2 +1,30 @@
-let id = "no";
+let id = "";
 // localStorage.clear();
+
+function handleData() {
+  document.getElementById("msg").innerHTML = "";
+  let name = document.getElementById("name").value;
+  if (name == "") {
+    document.getElementById("msg").innerHTML = "Please insert the data"; //Shows the message
+  } else {
+    if (id == "") {
+      let arr = JSON.parse(localStorage.getItem("crud"));
+      // console.log(arr); shows null value
+      if (!arr) {
+        let data = [name];
+        localStorage.setItem("crud", JSON.stringify(data));
+      } else {
+        arr.push(name);
+        localStorage.setItem("crud", JSON.stringify(arr));
+      }
+      document.getElementById("msg").innerHTML = "Data inserted";
+    } else {
+    }
+  }
+}
+
+function fetchData() {}
+
+function deleteData() {}
+
+function editData() {}
