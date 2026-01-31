5236d344a8e223406a3ce6d2fa26d2fe2057608e
Sanjaya Thapa <thapasanjay45643@gmail.com>
2026-01-31 21:52:01 +0545

    Edit operation

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let id = "";
+let id = "no";
 // localStorage.clear();
 fetchData();
 
@@ -8,7 +8,7 @@ function handleData() {
   if (name == "") {
     document.getElementById("msg").innerHTML = "Please insert the data"; //Shows the message
   } else {
-    if (id == "") {
+    if (id == "no") {
       let arr = JSON.parse(localStorage.getItem("crud"));
       // console.log(arr); shows null value
       if (!arr) {
@@ -20,7 +20,12 @@ function handleData() {
       }
       document.getElementById("msg").innerHTML = "Data inserted";
     } else {
+      let arr = JSON.parse(localStorage.getItem("crud"));
+      arr[id] = name;
+      localStorage.setItem("crud", JSON.stringify(arr));
+      document.getElementById("msg").innerHTML = "Data updated";
     }
+    document.getElementById("name").value = "";
     fetchData();
   }
 }
@@ -31,7 +36,7 @@ function fetchData() {
     let html = "";
     let sno = 1;
     for (let i in arr) {
-      html += `<tr><td>${sno}</td><td>${arr[i]}</td><td><a href ="javascript:void(0)" onclick ="deleteData(${i})">Delete</a></td></tr>`; //javascript:void(0) prevents reload in page
+      html += `<tr><td>${sno}</td><td>${arr[i]}</td><td><a href ="javascript:void(0)" onclick ="editData(${i})">Edit</a>&nbsp;<a href ="javascript:void(0)" onclick ="deleteData(${i})">Delete</a></td></tr>`; //javascript:void(0) prevents reload in page
     }
     document.getElementById("user").innerHTML = html;
   }
@@ -44,4 +49,8 @@ function deleteData(recordData) {
   fetchData();
 }
 
-function editData() {}
+function editData(recordData) {
+  id = recordData;
+  let arr = JSON.parse(localStorage.getItem("crud"));
+  document.getElementById("name").value = arr[recordData];
+}
